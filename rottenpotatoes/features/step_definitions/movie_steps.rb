ea158eb2fc810daa762e3b269c7ce2fef610fed0
John Smith Gaddy <jsgaddy1@gmail.com>
2016-11-24 08:21:44 +0000

    please work

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,17 +4,28 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
 end
+
+Then /the director of "(.*)" should be "(.*)"/ do |movie, director|
+  step %Q{I should see "#{movie}"}
+  step %Q{I should see "#{director}"}
+end
+
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
+Then /^the director of "([^"]*)" should be "([^"]*)"$/ do |arg1, arg2|
+   assert page.body =~ /#{arg1}.+Director.+#{arg2}/m
+   # express the regexp above with the code you wish you had
+end
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  
+  assert page.body =~ /#{e1}.+#{e2}/m
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,10 +36,15 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  if uncheck == "un"
+    rating_list.split(', ').each {|x| step %{I uncheck "ratings_{x}"}}
+  else
+    rating_lista.split(', ').each{|x| step %{I check "ratings_#{x}"}}
+  end  
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  rows = page.all('#movies tr').size-1
+  assert rows == Movie.count()
 end
